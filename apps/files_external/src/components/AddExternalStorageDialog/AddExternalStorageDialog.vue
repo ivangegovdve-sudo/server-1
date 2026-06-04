e3bf237eb9f2de00c2bc2b2a926defda60634c8c
Kent Delante <kent@delante.me>
2026-06-04 19:13:52 +0800

    fix(files_external): prevent dropdown heights from shrinking
    Signed-off-by: Kent Delante <kent@delante.me>

--- apps/files_external/src/components/AddExternalStorageDialog/AddExternalStorageDialog.vue
+++ apps/files_external/src/components/AddExternalStorageDialog/AddExternalStorageDialog.vue
@@ -93,13 +93,15 @@ watch(authMechanisms, () => {
 		<ApplicableEntities
 			v-if="isAdmin"
 			v-model:groups="internalStorage.applicableGroups"
-			v-model:users="internalStorage.applicableUsers" />
+			v-model:users="internalStorage.applicableUsers"
+			:class="$style.externalStorageDialog__dropdown" />
 
 		<NcSelect
 			v-model="backend"
 			:options="backends"
 			:disabled="!!(internalStorage.id && internalStorage.backend)"
 			:inputLabel="t('files_external', 'External storage')"
+			:class="$style.externalStorageDialog__dropdown"
 			label="name"
 			required />
 
@@ -108,6 +110,7 @@ watch(authMechanisms, () => {
 			:options="authMechanisms"
 			:disabled="!internalStorage.backend || authMechanisms.length <= 1 || !!(internalStorage.id && internalStorage.authMechanism)"
 			:inputLabel="t('files_external', 'Authentication')"
+			:class="$style.externalStorageDialog__dropdown"
 			label="name"
 			required />
 
@@ -145,5 +148,9 @@ watch(authMechanisms, () => {
 
 .externalStorageDialog__configuration {
 	margin-block: 0.5rem;
+}
+
+.externalStorageDialog__dropdown {
+	flex: 0 0 auto;
 }
 </style>
